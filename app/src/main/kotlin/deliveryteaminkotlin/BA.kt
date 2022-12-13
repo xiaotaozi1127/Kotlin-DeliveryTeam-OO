eef8634e57a4b82e42e32869441f00eec4b2d0d9
taohui <htao@thoughtworks.com>
2022-12-13 14:56:54 +0800

    ba need to prepare 3 cards at most

--- app/src/main/kotlin/deliveryteaminkotlin/BA.kt
+++ app/src/main/kotlin/deliveryteaminkotlin/BA.kt
@@ -6,15 +6,17 @@ class BA(name: String): Member(name) {
     override fun work() {
         if (team == null) return
 
-        val stories = team!!.getAllStories().filter { it.status == StoryStatus.BACKLOG }
-        val devs = team!!.getMembers { it is Dev }
-        if (stories.isEmpty() || devs.isEmpty()) return
+        val stories = team!!.getAllStories().filter { it.status == StoryStatus.BACKLOG }.subList(0, 3)
+        stories.forEach { it.status = StoryStatus.READY_FOR_DEV }
 
-        val count: Int = min(stories.size, devs.size)
+        val readyForDevStories = team!!.getAllStories().filter { it.status == StoryStatus.READY_FOR_DEV }
+        val devs = team!!.getMembers { it is Dev }
+        if (readyForDevStories.isEmpty() || devs.isEmpty()) return
+
+        val count: Int = min(readyForDevStories.size, devs.size)
         for (index in 0 until count) {
             val story = stories[index]
             val dev = devs[index]
-            story.status = StoryStatus.READY_FOR_DEV
             if (dev is Dev) {
                 dev.assignedStory = story
             }
--- app/src/test/kotlin/deliveryteaminkotlin/BATest.kt
+++ app/src/test/kotlin/deliveryteaminkotlin/BATest.kt
@@ -2,6 +2,8 @@ package deliveryteaminkotlin
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 class BATest {
     @Test fun ba_should_be_able_to_prepare_story_and_assign_card() {
@@ -16,5 +18,39 @@ class BATest {
         ba.work()
 
         assertEquals(story, dev.assignedStory)
+    }
+
+    @Test fun ba_should_be_able_to_prepare_3_stories_at_most() {
+        val team = Team()
+        val ba = BA("xixi")
+        val yanmin = Dev("yanmin")
+        val yunlong = Dev("yunlong")
+        val haotian = Dev("haotian")
+        val yanhua = Dev("yanhua")
+        team.assignMember(ba)
+        team.assignMember(yanmin)
+        team.assignMember(yunlong)
+        team.assignMember(haotian)
+        team.assignMember(yanhua)
+
+        val story1 = Story("story1")
+        val story2 = Story("story2")
+        val story3 = Story("story3")
+        val story4 = Story("story4")
+        team.assignStory(story1)
+        team.assignStory(story2)
+        team.assignStory(story3)
+        team.assignStory(story4)
+
+        ba.work()
+
+        assertEquals(StoryStatus.READY_FOR_DEV, story1.status)
+        assertEquals(StoryStatus.READY_FOR_DEV, story2.status)
+        assertEquals(StoryStatus.READY_FOR_DEV, story3.status)
+        assertEquals(StoryStatus.BACKLOG, story4.status)
+        assertNotNull(yanmin.assignedStory)
+        assertNotNull(yunlong.assignedStory)
+        assertNotNull(haotian.assignedStory)
+        assertNull(yanhua.assignedStory)
     }
 }
