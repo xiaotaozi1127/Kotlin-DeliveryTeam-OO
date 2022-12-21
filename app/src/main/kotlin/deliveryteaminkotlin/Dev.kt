1d07ee1cda075516f1606adcc6a51f615ac17d3d
taohui <htao@thoughtworks.com>
2022-12-21 08:26:29 +0800

    refactor code

--- app/src/main/kotlin/deliveryteaminkotlin/Dev.kt
+++ app/src/main/kotlin/deliveryteaminkotlin/Dev.kt
@@ -2,6 +2,12 @@ package deliveryteaminkotlin
 
 class Dev(name: String): Member(name) {
     var assignedStory: Story? = null
+    set(value) {
+        if (assignedStory != null && value != null) {
+            throw Exception("developer already assigned a story")
+        }
+        field = value
+    }
 
     override fun work() {
         if (team == null || assignedStory == null) return
